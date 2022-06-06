7eae407a6ee12e85bb1fe09672cc490f4d655f02
jatinkesharwani <jatinkesharwani360@gmail.com>
2022-06-06 16:41:13 +0530

    first commit

---
+++ backend/data/users.js
@@ -0,0 +1,8 @@
+const users=[{
+    name:"jatink",
+    email:"jatin@gmail.com",
+    password:"jatinnotes",
+    isAdmin:true,
+    pic:"file:///D:/project/notes-web-app/backend/data/profile.png"
+
+}]
